b3eb5c147c2fcb79a517874bac52389a1f05c202
Johan T. Halseth <johanth@gmail.com>
2019-05-01 17:32:28 +0200

    mobile/gen_bindings: add sub server proto support

--- mobile/gen_bindings.sh
+++ mobile/gen_bindings.sh
@@ -10,6 +10,30 @@ protoc -I/usr/local/include -I. \
        -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
        --plugin=protoc-gen-custom=$GOPATH/bin/promobile \
        --custom_out=./build \
-       --custom_opt="package_name=$pkg,target_package=$target_pkg" \
+       --custom_opt="package_name=$pkg,target_package=$target_pkg,listeners=lightning=lightningLis walletunlocker=walletUnlockerLis" \
        --proto_path=../lnrpc \
        rpc.proto
+
+for file in ../lnrpc/**/*.proto
+do
+    DIRECTORY=$(dirname ${file})
+    tag=$(basename ${DIRECTORY})
+    filename=$(basename ${file})
+    service=${filename%.proto}
+    build_tags="// +build $tag"
+    use_prefix="1"
+    lis="$service=lightningLis"
+
+    opts="package_name=$pkg,target_package=$target_pkg/$tag,build_tags=$build_tags,api_prefix=$use_prefix,listeners=$lis"
+
+    echo "Generating mobile protos from ${file}, with build tag ${tag}"
+
+    protoc -I/usr/local/include -I. \
+           -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+           -I../lnrpc \
+           --plugin=protoc-gen-custom=$GOPATH/bin/promobile \
+           --custom_out=./build \
+           --custom_opt="$opts" \
+           --proto_path=${DIRECTORY} \
+           ${file}
+done
