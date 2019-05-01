aa8ed2a68295ee88898220cb6609cfad85e08798
Johan T. Halseth <johanth@gmail.com>
2019-05-01 17:31:47 +0200

    mobile/gen_bindings: extract paths

--- mobile/gen_bindings.sh
+++ mobile/gen_bindings.sh
@@ -2,12 +2,14 @@
 
 mkdir -p build
 
+pkg="lndmobile"
+target_pkg="github.com/lightningnetwork/lnd/lnrpc"
+
 # Generate APIs by passing the parsed protos to ./gen
 protoc -I/usr/local/include -I. \
-       -I$GOPATH/src \
        -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
        --plugin=protoc-gen-custom=$GOPATH/bin/promobile \
        --custom_out=./build \
-       --custom_opt='package_name=lndmobile,target_package=github.com/lightningnetwork/lnd/lnrpc' \
+       --custom_opt="package_name=$pkg,target_package=$target_pkg" \
        --proto_path=../lnrpc \
        rpc.proto
