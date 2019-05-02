920ad976aaebdabb0775795b5e2b4cf33e73ee1e
Johan T. Halseth <johanth@gmail.com>
2019-05-02 10:22:39 +0200

    mobile/gen_bindings: add promobile version check

--- mobile/gen_bindings.sh
+++ mobile/gen_bindings.sh
@@ -2,13 +2,30 @@
 
 mkdir -p build
 
+# Check promobile version.
+promobileVersion="0.1"
+promobile=$(which promobile)
+if [ $promobile ]
+then
+        version=$($promobile -v)
+        if [ $version != $promobileVersion ]
+        then
+                echo "promobile version $promobileVersion required"
+                exit 1
+        fi
+        echo "Using plugin $promobile $version"
+else
+        echo "promobile not found"
+        exit 1
+fi
+
 pkg="lndmobile"
 target_pkg="github.com/lightningnetwork/lnd/lnrpc"
 
 # Generate APIs by passing the parsed protos to ./gen
 protoc -I/usr/local/include -I. \
        -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
-       --plugin=protoc-gen-custom=$GOPATH/bin/promobile \
+       --plugin=protoc-gen-custom=$promobile\
        --custom_out=./build \
        --custom_opt="package_name=$pkg,target_package=$target_pkg,listeners=lightning=lightningLis walletunlocker=walletUnlockerLis" \
        --proto_path=../lnrpc \
@@ -31,7 +48,7 @@ do
     protoc -I/usr/local/include -I. \
            -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
            -I../lnrpc \
-           --plugin=protoc-gen-custom=$GOPATH/bin/promobile \
+           --plugin=protoc-gen-custom=$promobile \
            --custom_out=./build \
            --custom_opt="$opts" \
            --proto_path=${DIRECTORY} \
